0164d2f44c9cb1ca527e285e93afe9b34effb2f5
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-09-19 10:58:31 +0700

    feat: add loading state

--- app/hn-react/src/components/CardList.js
+++ app/hn-react/src/components/CardList.js
@@ -3,17 +3,25 @@ import axios from 'axios'
 import { PageContext } from '../contexts/PageContext'
 import Card from './Card'
 import Pagination from './Pagination'
+import Loading from './Loading'
 
 const CardList = ({ type }) => {
-  const { news, page, dispatch } = useContext(PageContext)
+  const { news, loading, page, dispatch } = useContext(PageContext)
 
   useEffect(() => {
     fetchNews()
-      .then(({ data }) => dispatch({ type: 'SET_NEWS', payload: data }))
-      .catch(err => new Error(err))
+      .then(({ data }) => {
+        dispatch({ type: 'TOGGLE_LOADING' })
+        dispatch({ type: 'SET_NEWS', payload: data })
+      })
+      .catch(err => {
+        dispatch({ type: 'TOGGLE_LOADING' })
+        throw new Error(err)
+      })
   }, [type, page])
 
   async function fetchNews() {
+    dispatch({ type: 'TOGGLE_LOADING' })
     return await axios.get(`https://api.hnpwa.com/v0/${type}/${page}.json`)
   }
 
@@ -22,9 +30,7 @@ const CardList = ({ type }) => {
       <Pagination />
       <div className="news-list">
         <div className="view">
-          {news.map(n => (
-            <Card key={n.id} {...n} />
-          ))}
+          {loading ? <Loading /> : news.map(n => <Card key={n.id} {...n} />)}
         </div>
       </div>
     </>
