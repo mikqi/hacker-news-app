8bfe949d94fdaf98a85c590b3c292b77b0a4a9c6
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-09-22 22:27:58 +0700

    feat: hide next/prev

--- app/hn-vue/src/components/Pagination.js
+++ app/hn-vue/src/components/Pagination.js
@@ -3,18 +3,20 @@ import { mapState, mapMutations } from 'vuex'
 export default {
   name: 'Pagination',
   computed: {
-    ...mapState('news', ['page'])
+    ...mapState('news', ['page', 'menu'])
   },
   methods: {
     ...mapMutations('news', ['PREV_PAGE', 'NEXT_PAGE'])
   },
   render () {
-    const { page, PREV_PAGE, NEXT_PAGE } = this
+    const { page, menu, PREV_PAGE, NEXT_PAGE } = this
+    const maxPages =
+      menu.length > 0 ? menu.filter(m => window.location.pathname.includes(m.topic))[0].maxPages : 1
     return (
       <div class="news-list-nav">
-        <a onClick={() => PREV_PAGE()}>&lt; Prev</a>
+        {page <= 1 ? '' : <a onClick={() => PREV_PAGE()}>&lt; Prev</a>}
         {page}
-        <a onClick={() => NEXT_PAGE()}>Next &gt;</a>
+        {page < maxPages ? <a onClick={() => NEXT_PAGE()}>Next &gt;</a> : ''}
       </div>
     )
   }
